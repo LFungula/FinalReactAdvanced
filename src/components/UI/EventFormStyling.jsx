5f7b6a25578bb62615364569214d94c8317eaa76
LIN FUNGULA <LINDA.FUNGULA@GMAIL.COM>
2025-06-24 21:27:49 +0200

    Add EventFormStyling

---
+++ src/components/UI/EventFormStyling.jsx
@@ -0,0 +1,39 @@
+export const formStyle = {
+  display: "flex",
+  flexDirection: "column",
+  margin: "0.5rem",
+  padding: "0.5rem",
+  gap: "0.5rem",
+  width: "100%",
+};
+
+export const buttonStyle = {
+  padding: "0.5rem",
+  margin: "0.5rem",
+  backgroundColor: "blue",
+  color: "#fff",
+  borderRadius: "5px",
+  cursor: "pointer",
+};
+
+export const inputStyle = {
+  borderRadius: "5px",
+  width: "100%",
+};
+
+export const labelStyle = {
+  padding: "0.5rem",
+  margin: "0.5rem",
+  borderRadius: "5px",
+  backgroundColor: "seashell",
+  fontWeight: "500",
+};
+
+export const checkboxLabelStyle = {
+  display: "flex",
+  flexDirection: "column",
+  padding: "0.5rem",
+  margin: "0.5rem",
+  borderRadius: "5px",
+  backgroundColor: "seashell",
+};
